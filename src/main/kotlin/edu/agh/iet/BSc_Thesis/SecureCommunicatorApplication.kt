d20729d2599721819e006db16f047ea3326099f4
Jakub Bodera <jakub.bodera@gmail.com>
2020-04-28 21:54:34 +0200

    Initial commit: Fixed Spring Error

--- src/main/kotlin/edu/agh/iet/BSc_Thesis/SecureCommunicatorApplication.kt
+++ src/main/kotlin/edu/agh/iet/BSc_Thesis/SecureCommunicatorApplication.kt
@@ -1,9 +1,12 @@
 package edu.agh.iet.BSc_Thesis
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = [DataSourceAutoConfiguration::class]
+)
 class SecureCommunicatorApplication
 
 fun main(args: Array<String>) {
