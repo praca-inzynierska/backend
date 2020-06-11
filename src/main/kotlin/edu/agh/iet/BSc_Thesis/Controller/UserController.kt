b5444f752d1bc8e87907db5c9a9e0c5a87ac72a9
lzajac <lksz.zajac@gmail.com>
2020-06-11 21:28:48 +0200

    Added more info for users

--- src/main/kotlin/edu/agh/iet/BSc_Thesis/Controller/UserController.kt
+++ src/main/kotlin/edu/agh/iet/BSc_Thesis/Controller/UserController.kt
@@ -1,7 +1,9 @@
 package edu.agh.iet.BSc_Thesis.Controller
 
+import edu.agh.iet.BSc_Thesis.Controller.UserInfoResponse.Companion.toUserInfoResponse
 import edu.agh.iet.BSc_Thesis.Model.Entities.User
 import edu.agh.iet.BSc_Thesis.Repositories.UserRepository
+import edu.agh.iet.BSc_Thesis.Util.JwtUtils
 import edu.agh.iet.BSc_Thesis.Util.JwtUtils.generateToken
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
@@ -19,21 +21,40 @@ class UserController : BaseController() {
     fun register(@RequestBody registerRequest: RegisterRequest): LoginResponse {
         val userToCreate: User = registerRequest.userFromRequest()
         userRepository.save(userToCreate)
-        return LoginResponse(generateToken(userToCreate))
+        return LoginResponse(generateToken(userToCreate), userToCreate.username)
     }
 
     @CrossOrigin
     @PostMapping("/login")
     fun login(@RequestBody loginRequest: LoginRequest): LoginResponse {
-        var user = userRepository.getUserByUsernameAndPassword(username = loginRequest.username, password = loginRequest.password)
-        return LoginResponse(generateToken(user))
+        val user = userRepository.getUserByUsernameAndPassword(username = loginRequest.username, password = loginRequest.password)
+        return LoginResponse(generateToken(user), "${user.firstName} ${user.lastName}")
+    }
+
+    @CrossOrigin
+    @GetMapping("/users/info")
+    fun userInfo(@RequestHeader("Token") token: String): UserInfoResponse {
+        val user = JwtUtils.getUserFromToken(token)
+        return user.toUserInfoResponse()
     }
 
 }
 
 data class LoginResponse(
-        var token: String
+        var token: String,
+        var username: String
 )
+
+data class UserInfoResponse(
+        var username: String,
+        var isTeacher: Boolean
+) {
+    companion object {
+        fun User.toUserInfoResponse(): UserInfoResponse {
+            return UserInfoResponse(this.username, this.isTeacher)
+        }
+    }
+}
 
 data class LoginRequest(
         var username: String,
@@ -43,9 +64,11 @@ data class LoginRequest(
 data class RegisterRequest(
         var username: String,
         var password: String,
+        var firstName: String,
+        var lastName: String,
         var isTeacher: Boolean
 ) {
     fun userFromRequest(): User {
-        return User(username, password, isTeacher)
+        return User(username, password, firstName, lastName, isTeacher)
     }
 }
--- src/main/kotlin/edu/agh/iet/BSc_Thesis/Model/Entities/User.kt
+++ src/main/kotlin/edu/agh/iet/BSc_Thesis/Model/Entities/User.kt
@@ -8,15 +8,23 @@ import javax.persistence.*;
 data class User(
         @Id @GeneratedValue(strategy = GenerationType.AUTO)
         var id: Long = -1,
-
-        @Column(unique=true)
+        @Column(unique = true)
         var username: String = "",
-
+        var firstName: String = "",
+        var lastName: String = "",
         var password: String = "",
-
         var isTeacher: Boolean = false
 ) : Serializable {
-        constructor(username: String,
-                    password: String,
-                    isTeacher: Boolean = false) : this(id = -1, username = username, password = password, isTeacher = isTeacher)
+    constructor(username: String,
+                password: String,
+                firstName: String,
+                lastName: String,
+                isTeacher: Boolean = false)
+            : this(id = -1,
+            username = username,
+            password = password,
+            firstName = firstName,
+            lastName = lastName,
+            isTeacher = isTeacher)
+
 }
